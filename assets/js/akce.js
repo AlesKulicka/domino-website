e1018b6a6f42eb0e373e95e6bb575ed3aac1a0a1
Bambus3264 <adamspevak1@gmail.com>
2026-08-23 14:35:27 +0200

    zasady zpracovani osobnich udaju, siegfried

--- assets/js/akce.js
+++ assets/js/akce.js
@@ -4,6 +4,13 @@
   const eventGroups = {
     upcoming: [
       {
+        id: "siegfried-und-brunhlid",
+        title: "Siegfried und Brunhlid",
+        image: "assets/images/siegfried2026.jpeg",
+        summary: "SŠ + druhý stupeň ZŠ",
+        details: ["Představení proběhne ve spolupráci s brněnským divadelním souborem Domino Theater, diváky čeká ilustrované vyprávění bájného příběhu z cyklu legend o Nibelunzích. Vypravěč, který je zapojen do děje, provází diváky magickým příběhem odehrávajícím se na scéně. Produkce je určena divákům různých jazykových úrovní, představení je velmi vizuální a je koncipováno tak, aby děj pochopil i začátečník a bylo pro něj motivací k dalšímu studiu cizího jazyka. Pokročilejší studenti si budou moci otestovat své znalosti němčiny při poslechu mluveného projevu."]
+      },
+      {
         id: "lord-of-the-flies2",
         title: "Lord of the Flies",
         image: "assets/images/lordflies.jpg",
